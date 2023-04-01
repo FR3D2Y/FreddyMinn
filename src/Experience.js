8b93cc46babc5aba9049a71d1f0b44c046b6a1b2
FR3D2Y <68161521+FR3D2Y@users.noreply.github.com>
2023-04-02 02:24:10 +0630

    Update Experience.js

--- src/Experience.js
+++ src/Experience.js
@@ -23,7 +23,7 @@ export default function Experience()
                     width={ 2.5 }
                     height={ 1.65 }
                     intensity={ 65 }
-                    color={ '#ff6900' }
+                    color={ '#ffff' }
                     rotation={ [ - 0.1, Math.PI, 0 ] }
                     position={ [ 0, 0.55, - 1.15 ] }
                 />
