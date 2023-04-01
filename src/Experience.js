b635c5040bca0bab3a7fdf1eff516495db873a2c
FR3D2Y <68161521+FR3D2Y@users.noreply.github.com>
2023-04-02 03:33:34 +0630

    Update Experience.js

--- src/Experience.js
+++ src/Experience.js
@@ -40,7 +40,7 @@ export default function Experience()
                         position={ [ 0, 1.56, - 1.4 ] }
                         rotation-x={ - 0.256 }
                     >
-                    <iframe src="https://freddy-minn.github.io/" />
+                    <iframe src="https://freddy-minn.vercel.app/" />
                     </Html>
                 </primitive>
 
